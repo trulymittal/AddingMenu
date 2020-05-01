ef04a88a68d1ed6656d5659aac8fa2511f5ae29d
Truly Mittal <trulymittal@gmail.com>
2020-05-02 01:18:25 +0530

    one menu many activities

--- app/src/main/java/com/example/addingmenu/SettingsActivity.java
+++ app/src/main/java/com/example/addingmenu/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.addingmenu;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SettingsActivity extends MenuAppCompatActivity {
 
@@ -19,4 +22,12 @@ public class SettingsActivity extends MenuAppCompatActivity {
         return super.onPrepareOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_delete) {
+            Toast.makeText(this, "Settings Activtiy delete call", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
